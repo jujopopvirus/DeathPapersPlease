5e0512e8abaf8488e1ed7332219ed0566f1f6001
Jurou_WhiteKoihi <justinjajasalonga@gmail.com>
2025-10-04 23:40:42 +0800

    edited COD_ENUM

--- content/script/characterclasses/char_class.gd
+++ content/script/characterclasses/char_class.gd
@@ -1,5 +1,18 @@
 extends Node
 class_name Character_Information
 
+
+enum COD_ENUM {
+	HHH,
+	VEHICLE,
+	STUNTS,
+	SELFHARM,
+	CANNI,
+	MURDR,
+	POISON,
+	DISEASE,
+	WILDANMLS
+}
+
 @export var char_name : String
-@export var COD : String
+@export var COD : COD_ENUM
