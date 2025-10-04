5b980db47c1df656286e803be5fb2c209464dd57
Jurou_WhiteKoihi <justinjajasalonga@gmail.com>
2025-10-04 23:24:34 +0800

    Update char_class.gd

--- content/script/characterclasses/char_class.gd
+++ content/script/characterclasses/char_class.gd
@@ -1,11 +1,5 @@
 extends Node
+class_name Character_Information
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export var char_name : String
+@export var COD : String
