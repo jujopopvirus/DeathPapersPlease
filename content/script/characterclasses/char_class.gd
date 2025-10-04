26f6dadc06f362759884275d4c8de3952c94c2cb
Rombo-1 <aldrichellybillones@gmail.com>
2025-10-05 01:23:26 +0800

    ID ANIMATION
    AAAAAAA

--- content/script/characterclasses/char_class.gd
+++ content/script/characterclasses/char_class.gd
@@ -17,3 +17,14 @@ enum COD_ENUM { #Enumerator
 
 @export var char_name : String
 @export var COD : COD_ENUM
+
+@export var texture_id : Texture2D
+
+func _ready():
+	$TextureRect.texture = texture_id
+
+	
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		$TextureRect/AnimationPlayer.play_backwards("Show ID")
