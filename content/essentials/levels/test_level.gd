c03043c5cfa48fc82f0bb5f8fca4f5d6ffe9f836
Jurou_WhiteKoihi <justinjajasalonga@gmail.com>
2025-10-05 12:02:41 +0800

    AddedDialogues

--- content/essentials/levels/test_level.gd
+++ content/essentials/levels/test_level.gd
@@ -1,8 +1,1 @@
 extends Node2D
-class_name Interviewee_Character
-
-@export var Dialogue_Timeline : String = '' #<------------------ Name of the timeline to play
-
-func _ready() -> void:
-	if Dialogue_Timeline != null:
-		Dialogic.start(Dialogue_Timeline)
---
+++ content/script/characterclasses/interviewee_placeholder.gd
@@ -0,0 +1,15 @@
+extends Control
+class_name Interviewee_Character
+
+@export var Dialogue_Timeline : String = '' #<------------------ Name of the timeline to play
+
+@export_category("Interviewee Character")
+@export var Texture_Arrays : Array[Texture2D]
+@onready var Texture_Character : Sprite2D = %Texture_Body
+
+func _on_character_button_pressed() -> void:
+	if Dialogue_Timeline != null:
+		Dialogic.start(Dialogue_Timeline)
+
+func _ready() -> void:
+	Texture_Character.texture = Texture_Arrays[0]
