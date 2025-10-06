d44b7966aaf298eb6d852f3db29a52dacb6fc676
Jurou_WhiteKoihi <justinjajasalonga@gmail.com>
2025-10-06 18:43:30 +0800

    Updated Dialogue

--- content/script/characterclasses/char_class.gd
+++ content/script/characterclasses/char_class.gd
@@ -7,4 +7,8 @@ class_name Character_Information_Resource #library
 @export var character_id : Texture2D
 @export var occupation :String
 @export var final_verdict :String
+
+@export_category("Dialogues")
 @export var Dialogue_Timeline : String = '' #<------------------ Name of the timeline to play
+@export var HeavenTimeline : String = ''
+@export var HellTimeline : String = ''
--- content/script/matching/interview_position.gd
+++ content/script/matching/interview_position.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var Interviewers_Array : Array[PackedScene]
 
 func _ready() -> void:
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 	var new_waittime = randi_range(8, 15)
 	randomize()
 	%Timer.wait_time = new_waittime
@@ -12,6 +13,13 @@ func add_interviewer():
 	print(interviewer)
 	add_child(interviewer)
 
+func _on_dialogic_signal(argument:String):
+	match argument:
+		"Finish_Interview":
+			leave_interview_from_scene()
+
+func leave_interview_from_scene():
+	$WalkAnim.play_backwards("Walking Animation")
 
 func _on_timer_timeout() -> void:
 	print("Interviewer Added")
