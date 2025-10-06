f44b60fdc72ebb17ac14809f05f23c5126a61949
Jurou_WhiteKoihi <justinjajasalonga@gmail.com>
2025-10-06 19:12:46 +0800

    FinalScores

--- content/script/GlobalVariable.gd
+++ content/script/GlobalVariable.gd
@@ -1,23 +1,29 @@
 extends Node
 
-var Final_Score : int = 0
+var Final_Score : int = 10
 var Current_Character : Character_Information_Resource
 
 signal CheckingDocument_Done(FinalVerdict : String)
 
+func Go_To_Score():
+	print(Final_Score)
+
 func Check_Document(Input_Occupation : String, Input_CoD : String, Input_FinalVerdict : String) -> void:
 	if Input_Occupation == Current_Character.occupation:
 		print("Right!")
+		Final_Score += 10
 	else:
 		print("Wrong")
 	
 	if Input_CoD == Current_Character.cause_of_death:
 		print("Right!")
+		Final_Score += 10
 	else:
 		print("Wrong")
 	
 	if Input_FinalVerdict == Current_Character.final_verdict:
 		print("Right!")
+		Final_Score += 10
 	else:
 		print("Wrong")
 	
--- content/script/matching/interview_position.gd
+++ content/script/matching/interview_position.gd
@@ -8,10 +8,17 @@ func _ready() -> void:
 	randomize()
 	%Timer.wait_time = new_waittime
 
+var current_interviewer : Interviewee_Character = null
+
 func add_interviewer():
-	var interviewer = Interviewers_Array.pick_random().instantiate()
-	print(interviewer)
-	add_child(interviewer)
+	if not Interviewers_Array.is_empty():
+		current_interviewer = Interviewers_Array[0].instantiate()
+		print(current_interviewer)
+		Interviewers_Array.remove_at(0)
+		
+		add_child(current_interviewer)
+	else:
+		print("Day Finished!")
 
 func _on_dialogic_signal(argument:String):
 	match argument:
@@ -19,9 +26,16 @@ func _on_dialogic_signal(argument:String):
 			leave_interview_from_scene()
 
 func leave_interview_from_scene():
-	$WalkAnim.play_backwards("Walking Animation")
+	$WalkAnim.play_backwards("ExitAnimation")
 
 func _on_timer_timeout() -> void:
 	print("Interviewer Added")
 	$WalkAnim.play("Walking Animation")
 	add_interviewer()
+
+func _on_walk_anim_animation_finished(anim_name: StringName) -> void:
+	match anim_name:
+		"ExitAnimation":
+			current_interviewer.queue_free()
+			current_interviewer = null
+			%Timer.start()
